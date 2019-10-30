c97f30a7534ca5e582677ef95f62541fb86090a6
Emily Sousa <diassousa@yahoo.com.br>
2019-10-29 23:22:12 -0300

    Colocando um splash

--- src/components/splashScreen/index.js
+++ src/components/splashScreen/index.js
@@ -1,16 +1,66 @@
 import React from 'react';
-import { Image, ImageBackground } from 'react-native';
-import styles from './styles';
+// import react in our code.
 
-const splashScreen = () => (
-  <ImageBackground source={require('~/assets/images/BG.png')} style={{ width: '100%', height: '100%' }}>
-    <ImageBackground source={require('~/assets/images/PATTERN.png')} style={{ width: '100%', height: '100%' }}>
-        <Image
-      style={styles.orcHelmet}
-      source={require('~/assets/images/orc_capacete.png')}
-    />
-      </ImageBackground>
-  </ImageBackground>
-);
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  Image,
+  Animated,
+} from 'react-native';
+// import all the components we are going to use
 
-export default splashScreen;
+
+export default class App extends React.Component {
+  state = {
+    height: new Animated.Value(30), // Initial value for opacity: 0
+    width: new Animated.Value(10), // Initial value for opacity: 0
+  };
+
+  componentDidMount() {
+    Animated.timing(
+      this.state.width, // The animated value to drive
+      {
+        toValue: 360, // Animate to opacity: 1 (opaque)
+        duration: 10, // Make it take a while
+      },
+    ).start(); // Starts the animation
+    Animated.timing(
+      this.state.height, // The animated value to drive
+      {
+        toValue: 750, // Animate to opacity: 1 (opaque)
+        duration: 1000, // Make it take a while
+      },
+    ).start(); // Starts the animation
+  }
+
+  render() {
+    const { height } = this.state;
+    const { width } = this.state;
+
+    return (
+      <View style={styles.MainContainer}>
+        <ImageBackground source={require('~/assets/images/BG.png')} style={{ width: '100%', height: '100%' }}>
+          <ImageBackground source={require('~/assets/images/PATTERN.png')} style={{ width: '100%', height: '100%' }}>
+            <Animated.Image
+              source={require('~/assets/images/orc_capacete.png')}
+              style={{
+                width,
+                height,
+                position: 'relative',
+              }}
+            />
+          </ImageBackground>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+
+  MainContainer: {
+    flex: 1,
+    position: 'relative',
+  },
+});
