5fd0c8960ecd909e2cf88d24b1e4d49144a011e2
vitorAlves7 <vitor.alves07750@outlook.com>
2019-10-30 15:32:11 -0300

    A constant was made in routes for the creation of bottom navigation bar
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>
    Co-authored-by: Adriele Lopes <adrielelopes.al@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { colors } from '~/styles';
+
+import Membros from './pages/Membros/index'
 
 import Main from '~/pages/Main';
 
-const Routes = createAppContainer(createStackNavigator(
+const mainStack = createStackNavigator(
   { Main },
   {
     headerLayoutPreset: 'center',
@@ -17,6 +19,30 @@ const Routes = createAppContainer(createStackNavigator(
       headerTintColor: colors.duckYellow,
     },
   },
-));
+)
+
+const MembrosStack = createStackNavigator(
+  { Membros }
+)
+
+const stackBottomTabNavigator = createMaterialBottomTabNavigator(
+  {
+    Main: { screen: mainStack },
+    Membros: { screen: MembrosStack},
+  },
+  {
+    inicialRouteName: 'Home',
+    activeColor: '#f0edf6',
+    barStyle: {backgroundColor: '#694fad'}
+  }
+)
+
+const AppNavigator = createStackNavigator (
+  {
+    App : stackBottomTabNavigator,
+  }
+)
+
+const Routes = createAppContainer( AppNavigator);
 
 export default Routes;
