a96be3a0c88f5295e6093762470217ae406ce81f
vitorAlves7 <vitor.alves07750@outlook.com>
2019-11-03 21:14:11 -0300

    criar a barra de navegação inferior #12
    Added new package.json
    
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>
    Co-authored-by: Adriele Lopes  <adrielelopes.al@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import { colors } from '~/styles';
 
-import Membros from './pages/Membros/index'
+import Membros from './pages/Membros/index';
 
 import Main from '~/pages/Main';
 
@@ -19,30 +20,33 @@ const mainStack = createStackNavigator(
       headerTintColor: colors.duckYellow,
     },
   },
-)
+);
 
 const MembrosStack = createStackNavigator(
-  { Membros }
-)
+  { Membros },
+);
 
 const stackBottomTabNavigator = createMaterialBottomTabNavigator(
   {
     Main: { screen: mainStack },
-    Membros: { screen: MembrosStack},
+    Membros: { screen: MembrosStack },
   },
   {
     inicialRouteName: 'Home',
     activeColor: '#f0edf6',
-    barStyle: {backgroundColor: '#694fad'}
-  }
-)
+    barStyle: { backgroundColor: '#694fad' },
+    defaultNavigationOptions: {
+      tabBarVisible: 'off',
+    },
+  },
+);
 
-const AppNavigator = createStackNavigator (
+const AppNavigator = createStackNavigator(
   {
-    App : stackBottomTabNavigator,
-  }
-)
+    App: stackBottomTabNavigator,
+  },
+);
 
-const Routes = createAppContainer( AppNavigator);
+const Routes = createAppContainer(AppNavigator);
 
 export default Routes;
