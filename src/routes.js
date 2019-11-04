bb4983ba14a1225186f53aa8d4abf3edca8c4da4
vitorAlves7 <vitor.alves07750@outlook.com>
2019-11-04 10:00:28 -0300

    criar_a_barra_de_navegação_inferior#12
    we added icons in the bottom tab navigator
    
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>
    Co-authored-by: Adriele Lopes <adrielelopes.al@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconIon from 'react-native-vector-icons/Ionicons';
 import { colors } from '~/styles';
+
 
 import Membros from './pages/Membros/index';
 
 import Main from '~/pages/Main';
+
+import Activities from './pages/Activities';
+
+// import Tasks from './pages/Tasks';
+// import Profile from './pages/Profile';
+
+Icon.loadFont();
+IconMaterial.loadFont();
+IconIon.loadFont();
 
 const mainStack = createStackNavigator(
   { Main },
@@ -26,15 +39,76 @@ const MembrosStack = createStackNavigator(
   { Membros },
 );
 
+const ActivitiesSatck = createStackNavigator(
+  { Activities },
+);
+
+// const TasksStack = createStackNavigator(
+//   { Tasks },
+// );
+
+// const ProfileStack = createStackNavigator(
+//   { Profile },
+// );
+
+
 const stackBottomTabNavigator = createMaterialBottomTabNavigator(
   {
-    Main: { screen: mainStack },
-    Membros: { screen: MembrosStack },
+    Membros: {
+      screen: MembrosStack,
+      navigationOptions: () => ({
+        tabBarLabel: 'Membros',
+        tabBarIcon: ({ focused }) => (
+          <Icon name="group" type="Icon" size={20} color={focused ? '#7AAF50' : '#808080'} />
+        ),
+      }),
+    },
+    Inicio: {
+      screen: mainStack,
+      navigationOptions: () => ({
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ focused }) => (
+          <Icon name="home" type="Icon" size={20} color={focused ? '#7AAF50' : '#808080'} />
+        ),
+      }),
+    },
+    Activities: {
+      screen: ActivitiesSatck,
+      navigationOptions: () => ({
+        tabBarLabel: 'Activities',
+        tabBarIcon: ({ focused }) => (
+          <Icon name="line-chart" type="Icon" size={20} color={focused ? '#7AAF50' : '#808080'} />
+        ),
+      }),
+    },
+    // Tarefas: {
+    //   screen: mainStack,
+    //   navigationOptions: () => ({
+    //     tabBarLabel: 'Tarefas',
+    //     tabBarIcon: ({ focused }) => (
+    //       <Icon name="home" type="FontAwesome" size={20} color={focused ? '#7AAF50' : '#808080'} />
+    //     ),
+    //   }),
+    // },
+    // Perfil: {
+    //   screen: mainStack,
+    //   navigationOptions: () => ({
+    //     tabBarLabel: 'Perfil',
+    //     tabBarIcon: ({ focused }) => (
+    //       <Icon name="home" type="FontAwesome" size={20} color={focused ? '#7AAF50' : '#808080'} />
+    //     ),
+    //   }),
+    // },
   },
   {
     inicialRouteName: 'Home',
-    activeColor: '#f0edf6',
-    barStyle: { backgroundColor: '#694fad' },
+    activeColor: '#7AAF50',
+    barStyle: { backgroundColor: '#DCDCDC' },
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: true,
+
+    },
     defaultNavigationOptions: {
       tabBarVisible: 'off',
     },
