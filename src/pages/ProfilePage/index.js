3af628afd24bf7ab1e52be6fcd6e7b87ca7464a5
Mateus Oliveira <o.mateus.p@gmail.com>
2019-11-06 13:26:05 -0300

    Opacidade_de_toque_#17

--- src/pages/ProfilePage/index.js
+++ src/pages/ProfilePage/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Platform,
+  TouchableOpacity,
+} from 'react-native';
 
 import { colors } from '~/styles';
 
@@ -61,7 +68,9 @@ export default class Profile extends Component {
             {'\n'}
           </Text>
         </View>
-        <Text style={styles.textCalendario}>Ver meu calendário</Text>
+        <TouchableOpacity>
+          <Text style={styles.textCalendario}>Ver meu calendário</Text>
+        </TouchableOpacity>
       </View>
     );
   }
