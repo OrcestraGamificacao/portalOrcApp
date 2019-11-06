6a3e192df8ba85766810515cc4a22f5882e71cd6
iurisevero <nobreiuri@gmail.com>
2019-11-06 00:39:35 -0300

    Adicionando Pop up ao clicar no +

--- src/pages/TasksPage/index.js
+++ src/pages/TasksPage/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  Image, ScrollView, Text, StyleSheet, TouchableOpacity,
+  Image, ScrollView, Text, StyleSheet, TouchableOpacity, View,
 } from 'react-native';
+// import Dialog from 'react-native-dialog';
+import Dialog, { DialogContent, DialogTitle, DialogButton } from 'react-native-popup-dialog';
 import { colors } from '~/styles';
 import TasksNavBar from '~/components/tasksNavBar';
 import TasksVencidas from '~/components/tasksVencidas';
@@ -9,54 +11,83 @@ import TasksHoje from '~/components/tasksHoje';
 import TasksAmanha from '~/components/tasksAmanha';
 import TasksProximos from '~/components/tasksProximos';
 import styles from './styles';
-// comentario
-const Main = () => (
-  <ScrollView>
-    <TasksNavBar />
-    <TasksVencidas />
-    <TasksHoje />
-    <TasksAmanha />
-    <TasksProximos />
-  </ScrollView>
-);
 
-Main.navigationOptions = {
-  title: 'Tarefas',
-  headerLeft: (
-    <Image
-      style={styles.logoOrcStyle}
-      source={{
-        uri:
-          'https://user-images.githubusercontent.com/54643355/67619110-d119dc00-f7cd-11e9-9377-40b7c21424fe.png',
-      }}
-    />
-  ),
-  headerRight: (
-    <TouchableOpacity
-      style={styles.addButtonStyle}
-    >
-      <Text style={styles.addButtonTextStyle}>+</Text>
+export default class Main extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Tarefas',
+    headerLeft: (
+      <Image
+        style={styles.logoOrcStyle}
+        source={{
+          uri:
+            'https://user-images.githubusercontent.com/54643355/67619110-d119dc00-f7cd-11e9-9377-40b7c21424fe.png',
+        }}
+      />
+    ),
+    headerRight: (
+      <TouchableOpacity
+        style={styles.addButtonStyle}
 
-    </TouchableOpacity>
-  ),
-  headerBackTitleVisible: false,
-  headerTintColor: '#FFF',
-  headerBackground: (
-    <Image
-      style={StyleSheet.absoluteFill}
-      source={{
-        uri: 'https://i.imgur.com/oPgigXi.jpg',
-      }}
-    />
-  ),
-  headerStyle: {
-    backgroundColor: colors.greenOrcLight,
-    textAlign: 'center',
-  },
-  headerTitleStyle: {
-    fontFamily: 'monospace',
-    fontSize: 30,
-  },
-};
+        onPress={navigation.getParam('visible')}
+      >
+        <Text style={styles.addButtonTextStyle}>+</Text>
+      </TouchableOpacity>
+    ),
+    headerBackTitleVisible: false,
+    headerTintColor: '#FFF',
+    headerBackground: (
+      <Image
+        style={StyleSheet.absoluteFill}
+        source={{
+          uri: 'https://i.imgur.com/oPgigXi.jpg',
+        }}
+      />
+    ),
+    headerStyle: {
+      backgroundColor: colors.greenOrcLight,
+      textAlign: 'center',
+    },
+    headerTitleStyle: {
+      fontFamily: 'monospace',
+      fontSize: 30,
+    },
+  });
 
-export default Main;
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  componentDidMount() {
+    this.props.navigation.setParams({ visible: this.handleOnPress });
+  }
+
+  handleOnPress = () => this.setState({ visible: true })
+
+  render() {
+    return (
+      <ScrollView>
+        <Dialog
+          visible={this.state.visible}
+          onTouchOutside={() => {
+            this.setState({ visible: false });
+          }}
+        >
+          <DialogContent>
+            <Text>
+            Agora falta personalizar isso tudo.
+            </Text>
+          </DialogContent>
+        </Dialog>
+
+        <TasksNavBar />
+        <TasksVencidas />
+        <TasksHoje />
+        <TasksAmanha />
+        <TasksProximos />
+      </ScrollView>
+    );
+  }
+}
