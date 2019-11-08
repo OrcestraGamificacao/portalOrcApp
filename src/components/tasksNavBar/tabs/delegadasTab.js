1c04a49eb26327a3b5b8273fc9a339f836e629e5
ciro-c <ciro.fga@gmail.com>
2019-11-08 15:31:09 -0300

    Arrumando o titulo das checkbox da pagina delegadas #27
    Co-authored-by: Iuri Severo <nobreiuri@gmail.com>

--- src/components/tasksNavBar/tabs/delegadasTab.js
+++ src/components/tasksNavBar/tabs/delegadasTab.js
@@ -2,19 +2,16 @@ import React, { Component } from 'react';
 import {
   ScrollView,
 } from 'react-native';
-import TasksVencidas from '~/components/tasksVencidas';
-import TasksHoje from '~/components/tasksHoje';
-import TasksAmanha from '~/components/tasksAmanha';
-import TasksProximos from '~/components/tasksProximos';
+
+import TaskDelegada from '~/components/tasksDelegada';
 
 export default class Delegadas extends Component {
   render() {
     return (
       <ScrollView>
-        <TasksVencidas />
-        <TasksHoje />
-        <TasksAmanha />
-        <TasksProximos />
+        <TaskDelegada />
+        <TaskDelegada />
+        <TaskDelegada />
       </ScrollView>
     );
   }
