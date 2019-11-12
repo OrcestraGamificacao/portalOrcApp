b64e1c2ea05265bf5ac94eb8356530515b3a5f11
Fellipe Araujo <araujo.fellipe@hotmail.com>
2019-11-11 21:44:01 -0300

    Stylization Activity page.

--- src/components/Atividades/styles.js
+++ src/components/Atividades/styles.js
@@ -1,20 +1,35 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-
-    image: {
-        width: 36,
-        height: 36,
-        borderRadius: 36,
-    },
-    name:{
-        fontFamily: 'Rokkitt',
-        fontSize: 12,
-        color: '#000',             
-        fontStyle: 'normal',
-        fontWeight: 'normal',
-    }
-
+  image: {
+    width: 36,
+    height: 36,
+    borderRadius: 36,
+  },
+  name: {
+    fontFamily: 'Rokkitt',
+    fontSize: 12,
+    color: '#000',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+  },
+  notify: {
+    fontFamily: 'Rokkitt',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 12,
+    color: '#000',
+  },
+  notify_details: {
+    fontFamily: 'Rokkitt',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 10,
+    color: '#474535',
+  },
+  activity: {
+    flexDirection: 'column',
+  },
 });
 
 export default styles;
