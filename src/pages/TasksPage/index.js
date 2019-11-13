3da0d029f12c74a21ee0de949b4004408c95734b
Estevao Reis <estevaoreis1587@gmail.com>
2019-11-13 10:22:18 -0300

    Modificação feita com o npm install

--- src/pages/TasksPage/index.js
+++ src/pages/TasksPage/index.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Image, ScrollView, Text, StyleSheet, TouchableOpacity, View,
+  Image, ScrollView, Text, StyleSheet, TouchableOpacity,
 } from 'react-native';
-// import Dialog from 'react-native-dialog';
-import Dialog, { DialogContent, DialogTitle, DialogButton } from 'react-native-popup-dialog';
 import { colors } from '~/styles';
 import TasksNavBar from '~/components/tasksNavBar';
 import TasksVencidas from '~/components/tasksVencidas';
@@ -12,82 +10,54 @@ import TasksAmanha from '~/components/tasksAmanha';
 import TasksProximos from '~/components/tasksProximos';
 import styles from './styles';
 
-export default class Main extends Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: 'Tarefas',
-    headerLeft: (
-      <Image
+const Main = () => (
+  <ScrollView>
+    <TasksNavBar />
+    <TasksVencidas />
+    <TasksHoje />
+    <TasksAmanha />
+    <TasksProximos />
+  </ScrollView>
+);
+
+Main.navigationOptions = {
+  title: 'Tarefas',
+  headerLeft: (
+    <Image
         style={styles.logoOrcStyle}
         source={{
           uri:
             'https://user-images.githubusercontent.com/54643355/67619110-d119dc00-f7cd-11e9-9377-40b7c21424fe.png',
         }}
       />
-    ),
-    headerRight: (
-      <TouchableOpacity
+  ),
+  headerRight: (
+    <TouchableOpacity
         style={styles.addButtonStyle}
-
-        onPress={navigation.getParam('visible')}
       >
         <Text style={styles.addButtonTextStyle}>+</Text>
       </TouchableOpacity>
-    ),
-    headerBackTitleVisible: false,
-    headerTintColor: '#FFF',
-    headerBackground: (
-      <Image
+  ),
+  headerBackTitleVisible: false,
+  headerTintColor: '#FFF',
+  headerBackground: (
+    <Image
         style={StyleSheet.absoluteFill}
         source={{
           uri: 'https://i.imgur.com/oPgigXi.jpg',
         }}
       />
-    ),
-    headerStyle: {
-      backgroundColor: colors.greenOrcLight,
-      textAlign: 'center',
-    },
-    headerTitleStyle: {
-      fontFamily: 'monospace',
-      fontSize: 30,
-    },
-  });
+  ),
+  headerStyle: {
+    backgroundColor: colors.greenOrcLight,
+    textAlign: 'center',
+  },
+  headerTitleStyle: {
+    fontFamily: 'monospace',
+    fontSize: 30,
+  },
+};
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-    };
-  }
+export default Main;
 
-  componentDidMount() {
-    this.props.navigation.setParams({ visible: this.handleOnPress });
-  }
-
-  handleOnPress = () => this.setState({ visible: true })
-
-  render() {
-    return (
-      <ScrollView>
-        <Dialog
-          visible={this.state.visible}
-          onTouchOutside={() => {
-            this.setState({ visible: false });
-          }}
-        >
-          <DialogContent>
-            <Text>
-            Agora falta personalizar isso tudo.
-            </Text>
-          </DialogContent>
-        </Dialog>
-
-        <TasksNavBar />
-        <TasksVencidas />
-        <TasksHoje />
-        <TasksAmanha />
-        <TasksProximos />
-      </ScrollView>
-    );
-  }
-}
+// lol
