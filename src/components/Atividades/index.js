d954e89e8a50533f6d625fb8b7b0849ceb64c145
Brian Pina de Sá <brian.pina2001@gmail.com>
2019-11-19 14:28:03 -0300

    Fix #13 #15 Filtro das atividades e melhoria da estilização

--- src/components/Atividades/index.js
+++ src/components/Atividades/index.js
@@ -9,51 +9,51 @@ const mocks = [
     name: 'Brian Pina',
     notify: ' criou uma nova área de trabalho',
     image: require('../../../assets/Orc_estra_capacete_colorido.png'),
-    date: new Date("Nov 19 2019"),
-    where: "Diretoria de projetos"
+    date: new Date('Nov 19 2019'),
+    where: 'Diretoria de projetos',
   },
   {
     name: 'Daniela Soares',
     notify: ' criou uma nova área de trabalho',
     image: require('../../../assets/Orc_estra_capacete_colorido.png'),
-    date: new Date("Nov 16 2019"),
-    where: "Diretoria de operações"
+    date: new Date('Nov 16 2019'),
+    where: 'Diretoria de operações',
   },
   {
     name: 'Eric',
     notify: ' criou uma nova área de trabalho',
     image: require('../../../assets/Orc_estra_capacete_colorido.png'),
-    date: new Date("Nov 16 2019"),
-    where: "Diretoria de marketing"
+    date: new Date('Nov 16 2019'),
+    where: 'Diretoria de marketing',
   },
   {
     name: 'Fellipe Araujo',
     notify: ' criou uma nova área de trabalho',
     image: require('../../../assets/Orc_estra_capacete_colorido.png'),
-    date: new Date("Oct 20 2019"),
-    where: "Diretoria de negócios"
+    date: new Date('Oct 20 2019'),
+    where: 'Diretoria de negócios',
   },
 ];
 
-const months = ["Jan", "Fev", "Mar", "Abr", "Maio", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
-let now = new Date();
-let hoje = mocks.filter(act => {
-  let days = Math.round((Date.parse(now) - Date.parse(act.date) ) / (24*60*60*1000));
+const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+const now = new Date();
+const hoje = mocks.filter(act => {
+  const days = Math.round((Date.parse(now) - Date.parse(act.date)) / (24 * 60 * 60 * 1000));
   return days <= 1;
 });
-let semana = mocks.filter(act => {
-  let days = Math.round((Date.parse(now) - Date.parse(act.date) ) / (24*60*60*1000));
+const semana = mocks.filter(act => {
+  const days = Math.round((Date.parse(now) - Date.parse(act.date)) / (24 * 60 * 60 * 1000));
   return days > 1 && days <= 7;
 });
-let mes = mocks.filter(act => {
-  let days = Math.round((Date.parse(now) - Date.parse(act.date) ) / (24*60*60*1000));
+const mes = mocks.filter(act => {
+  const days = Math.round((Date.parse(now) - Date.parse(act.date)) / (24 * 60 * 60 * 1000));
   return days > 7 && days <= 31;
 });
 
 const Activity = () => {
   return (
     <View style={styles.container}>
-      <Text>Hoje</Text>
+      <Text style={styles.date}>Hoje</Text>
       {hoje.map((member, index) => (
         <View key={index} style={styles.activity}>
           <View>
@@ -70,7 +70,8 @@ const Activity = () => {
           </View>
         </View>
       ))}
-      <Text>Essa semana</Text>
+      <View style={styles.line} />
+      <Text style={styles.date}>Essa semana</Text>
       {semana.map((member, index) => (
         <View key={index} style={styles.activity}>
           <View>
@@ -87,7 +88,8 @@ const Activity = () => {
           </View>
         </View>
       ))}
-      <Text>Esse mês</Text>
+      <View style={styles.line} />
+      <Text style={styles.date}>Esse mês</Text>
       {mes.map((member, index) => (
         <View key={index} style={styles.activity}>
           <View>
--- src/components/Atividades/styles.js
+++ src/components/Atividades/styles.js
@@ -7,39 +7,56 @@ const styles = StyleSheet.create({
     borderRadius: 36,
   },
   name: {
-    fontFamily: 'monospace',
+    fontFamily: 'Roboto',
     fontSize: 12,
     color: '#000',
     fontStyle: 'normal',
     fontWeight: 'normal',
   },
   notify: {
-    fontFamily: 'monospace',
+    fontFamily: 'Roboto',
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 12,
     color: '#000',
   },
   notify_details: {
-    fontFamily: 'monospace',
+    fontFamily: 'Roboto',
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 10,
     color: '#474535',
     marginTop: 4,
   },
-  container: {
-    width: '95%',
-  },
   activity: {
     flexDirection: 'row',
     padding: 7,
     marginTop: 11,
     alignItems: 'center',
+    width: '92%',
+    marginHorizontal: 'auto',
   },
   container_notifies: {
     width: '90%',
     marginLeft: 9,
+  },
+  date: {
+    fontSize: 20,
+    marginLeft: 7,
+    marginTop: 5,
+    fontFamily: 'Roboto',
+    alignSelf: 'flex-start',
+  },
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  line: {
+    backgroundColor: 'rgba(0, 0, 0, .15)',
+    height: 1,
+    width: '100%',
+    marginTop: 21,
+    marginBottom: 10,
   },
 });
 
